9cad4558e3a89f05c4fdd30731ea8749f5624d94
LolaDeveloper <82631715+LolaDeveloper@users.noreply.github.com>
2021-11-11 22:49:06 +0900

    Added Footer with GitHub URL

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import Footer from "./Footer";
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather App</div>;
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">Weather App</div>
+      <Footer />
+    </div>
+  );
 }
-
-export default App;
---
+++ src/Footer.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div>
+      <footer className="container">
+        <p className="mt-5">
+          😎This project was coded by{" "}
+          <a
+            href="https://funmilola-alice-adeniran-portfolio.netlify.app/"
+            title="View source code"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Funmilola
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/LolaDeveloper/my-new-react-weather-app"
+            title="View source code"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-sourced on GitHub
+          </a>
+        </p>
+      </footer>
+    </div>
+  );
+}
