6e842e98f85a6833e4e76b91b85f0d2c49b6bf2d
LolaDeveloper <82631715+LolaDeveloper@users.noreply.github.com>
2021-11-12 20:52:12 +0900

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <div className="container">Weather App</div>
+      <div className="container">
+        <h1> Weather App <h1/>
+      </div>
       <Footer />
     </div>
   );
