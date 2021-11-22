186e624b33e4ea320aa7763b73ee90cced52f74d
LolaDeveloper <82631715+LolaDeveloper@users.noreply.github.com>
2021-11-22 22:13:35 +0900

    Integrated daily forecast open weather API into the weather forecast component

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,8 +1,16 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import axios from "axios";
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
+  function handleResponse(response) {}
+
+  const apiKey = "224e79ec9be151ffbe6eb3b4e7ab3ada";
+  let longitude = props.coordinates.lon;
+  let latitude = props.coordinates.lat;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude={part}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
   return (
     <div className="WeatherForecast">
       <div className="row">
