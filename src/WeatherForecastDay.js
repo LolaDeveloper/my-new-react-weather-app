988984705a6a99a3c20b2c64c354917d89373781
LolaDeveloper <82631715+LolaDeveloper@users.noreply.github.com>
2021-11-22 23:43:30 +0900

    created WeatherForecastDay.js to take care of daily weather forecast

---
+++ src/WeatherForecastDay.js
@@ -0,0 +1,40 @@
+import React from "react";
+import WeatherIcon from "./WeatherIcon";
+
+export default function WeatherForecastDay(props) {
+  function maxTemperature() {
+    let temperature = Math.round(props.data.temp.max);
+    return `${temperature}°`;
+  }
+  function minTemperature() {
+    let temperature = Math.round(props.data.temp.min);
+    return `${temperature}°`;
+  }
+
+  function day() {
+    let date = new Date(props.data.dt * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    return days[day];
+  }
+
+  return (
+    <div>
+      <div className="WeatherForecastDay">{day()}</div>
+      <WeatherIcon
+        code={props.data.weather[0].icon}
+        size={36}
+        color="#8F49FF"
+      />
+      <div className="WeatherForecastTemperatures">
+        <span className="WeatherForecastTemperatureMax">
+          {maxTemperature()}°{" "}
+        </span>
+        <span className="WeatherForecastTemperatureMin">
+          {" "}
+          {minTemperature()}°{" "}
+        </span>
+      </div>
+    </div>
+  );
+}
